9780bf38b320868c7ee2edfa099710a85a2cf2e0
Lekan Ojulowo <lekanojulowo@gmail.com>
2019-11-18 18:11:51 +0100

    Updates

---
+++ precache-manifest.10ce409ec281a751c03c9b015a6b0dd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a653ba8ae9c91cdd451780f40fb7c7e",
+    "url": "/teamwork_frontend/index.html"
+  },
+  {
+    "revision": "e17266534ac2a7cd78ea",
+    "url": "/teamwork_frontend/static/css/main.ea520c2e.chunk.css"
+  },
+  {
+    "revision": "7902d05b68517764d3ba",
+    "url": "/teamwork_frontend/static/js/2.5d9e4fa7.chunk.js"
+  },
+  {
+    "revision": "e17266534ac2a7cd78ea",
+    "url": "/teamwork_frontend/static/js/main.bd1dc445.chunk.js"
+  },
+  {
+    "revision": "c15a3c7aa8e4a65ab515",
+    "url": "/teamwork_frontend/static/js/runtime-main.1992b8a5.js"
+  },
+  {
+    "revision": "394e8a6b9feed4f8182952833cacf972",
+    "url": "/teamwork_frontend/static/media/logo.394e8a6b.jpg"
+  }
+]);
